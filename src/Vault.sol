30d4557a91a741a2dd23e9cf8aea1c8b1939adc6
virtual <spirited_tree@protonmail.com>
2022-01-20 13:29:52 +0000

    🐳 wrote code ig

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,10 +1,52 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.11;
 
-contract Vault {
-    function init() public {}
+import { ERC20 } from 'solmate/tokens/ERC20.sol';
+
+contract Vault is ERC20 {
+
+    address immutable controller;
+
+    uint256 public fee;
+
+    uint256 public maxFee;
+
+    uint256 public basePrice;
+
+    uint256 public reservePrice;
+
+    uint256 public supply;
+
+    bool public isInitialized;
+
+
+    constructor(address _controller, string memory _name, string memory _symbol) ERC20(_name, _symbol, 18) {
+        controller = _controller;
+        isInitialized = false;
+    }
+
+    function init(uint256 _fee, uint256 _basePrice, uint256 _reservePrice) public {
+        require(msg.sender == controller && isInitialized == false && _fee < maxFee);
+
+        fee = _fee;
+        basePrice = _basePrice;
+        reservePrice = _reservePrice;
+
+        isInitialized = true;
+    }
+
     function fractionalize() public {}
-    function start() public {}
+
+    function startAuction() public {}
+
     function bid() public {}
+
     function claimFees() public {}
+
+    function updateFee(uint256 _newFee) public {
+        require(msg.sender == controller && _newFee < maxFee);
+
+        fee = _newFee;
+    }
+
 }
